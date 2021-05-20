e2cb7a6fbfec68f4fcaeebf935cddb4c1c4b080f
Durgesh <durgeshkumar08@hotmail.com>
2021-05-20 23:04:15 +0200

    modified

--- gnome.sh
+++ gnome.sh
@@ -4,13 +4,15 @@ sudo timedatectl set-ntp true
 sudo hwclock --systohc
 
 #sudo reflector -c Germany -a 12 --sort rate --save /etc/pacman.d/mirrorlist
-#sudo pacman -Syy
-
+sudo pacman -Syy
+read -p "pacman sync"
 sudo firewall-cmd --add-port=1025-65535/tcp --permanent
 sudo firewall-cmd --add-port=1025-65535/udp --permanent
 sudo firewall-cmd --reload
 
-git clone https://aur.archlinux.org/paru-bin
+read -p "firewall"
+
+#git clone https://aur.archlinux.org/paru-bin
 # cd pikaur/
 # makepkg -si --noconfirm
 
@@ -21,12 +23,17 @@ git clone https://aur.archlinux.org/paru-bin
 #pikaur -S --noconfirm auto-cpufreq
 #sudo systemctl enable --now auto-cpufreq
 
-sudo pacman -S gdm gnome gnome-extra firefox gnome-tweaks gnome-software-packagekit-plugin archlinux-appstream-data  simplescreenrecorder arc-gtk-theme arc-icon-theme obs-studio vlc
+# gnome extra  - not included games
+sudo pacman -S lightdm gdm gnome sysprof swell-foop polari hitori gnome-usage gnome-tweaks gnome-todo gnome-sudoku gnome-sound-recorder gnome-robots gnome-nettool gnome-multi-writer gnome-games gnome-devel-docs gnome-connections gnome-code-assistance gnome-chess gnome-builder glade ghex geary dconf-editor gnome-software-packagekit-plugin
+read -p "gnome install"
 
-sudo flatpak install -y spotify
-sudo flatpak install -y kdenlive
+sudo pacman -S  firefox archlinux-appstream-data  simplescreenrecorder arc-gtk-theme arc-icon-theme obs-studio vlc
 
-sudo systemctl enable gdm
+
+#sudo flatpak install -y spotify
+#sudo flatpak install -y kdenlive
+
+sudo systemctl enable lightdm
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
 #sleep 5
 #sudo reboot
--- install-uefi.sh
+++ install-uefi.sh
@@ -4,26 +4,30 @@ ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 hwclock --systohc
 sed -i '177s/.//' /etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "LANG=en_UK.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=uk" >> /etc/vconsole.conf
 echo "durgesh-pc" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 durgesh-pc.localdomain durgesh-pc" >> /etc/hosts
 echo root:password | chpasswd
+read -p "local set"
 
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
 pacman -S grub grub-btrfs efibootmgr os-prober networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi 
+read -p "set 1 install"
 pacman -S xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack 
+read -p "set 2 install"
 pacman -S bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
-
+read -p "set 3 install"
 # pacman -S --noconfirm xf86-video-amdgpu
 pacman -S nvidia nvidia-utils nvidia-settings xdg-desktop-portal-impl
-
+read -p "nvidia install"
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
+read -p "grub config"
 
 systemctl enable NetworkManager
 systemctl enable bluetooth
@@ -36,10 +40,10 @@ systemctl enable fstrim.timer
 systemctl enable libvirtd
 systemctl enable firewalld
 systemctl enable acpid
-
+read -p "enable service"
 useradd -m durgesh
 echo durgesh:password | chpasswd
-usermod -aG libvirt durgesh
+usermod -aG wheel,libvirt durgesh
 
 echo "durgesh ALL=(ALL) ALL" >> /etc/sudoers.d/durgesh
 
